5c7b872d79fab75020862743ebca0643ed669ebc
liuji <1546243245@qq.com>
2022-11-11 06:11:17 +0800

    add type.go, chan.go

--- starter/main.go
+++ starter/main.go
@@ -14,4 +14,16 @@ func main() {
 	fmt.Println("hello", str)
 	fmt.Println(mymath.Sqrt(100.0))
 	fmt.Println(mymath.Mygcd(10, 5))
+	array := [...]int{10, 20, 30, 40} // array
+	slice := []int{10, 20, 30, 40}    // slice
+	for _, v := range array {
+		fmt.Println(v)
+	}
+	for _, v := range slice {
+		fmt.Println(v)
+	}
+	dict := make(map[string]int)
+	dict["hello"] = 2
+	TestType()
+	TestChan()
 }
---
+++ starter/type.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type user struct {
+	name       string
+	email      string
+	ext        int
+	privileged bool
+}
+
+type admin struct {
+	person user
+	level  string
+}
+
+func (u user) notify() {
+	fmt.Printf("Sending User Email To %s<%s>\n", u.name, u.email)
+}
+
+func (u *user) changeEmail(email string) {
+	u.email = email
+}
+
+func TestType() {
+	lisa := user{"lisa", "lisa@email.com", 123, true}
+	bob := user{name: "bob", email: "bob@email.com", ext: 234, privileged: false}
+	// fmt.Println(lisa)
+	fred := admin{
+		person: user{
+			name:       "Lisa",
+			email:      "lisa@email.com",
+			ext:        123,
+			privileged: true,
+		},
+		level: "super",
+	}
+	lisa.notify();
+	bob.notify()
+	fred.person.notify()
+	lisa.changeEmail("myemail@email.com")
+}
