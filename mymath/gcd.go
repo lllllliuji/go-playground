426af94cd78c5c7468dded70654b27ec67d21c1b
liuji <1546243245@qq.com>
2022-11-11 01:42:49 +0800

    gcd

---
+++ mymath/gcd.go
@@ -0,0 +1,8 @@
+package mymath
+
+func Mygcd(a , b int) int {
+	if b == 0 {
+		return a
+	}
+	return Mygcd(b, a % b)
+}
--- starter/main.go
+++ starter/main.go
@@ -13,4 +13,5 @@ func main() {
 	str := "world"
 	fmt.Println("hello", str)
 	fmt.Println(mymath.Sqrt(100.0))
+	fmt.Println(mymath.Mygcd(10, 5))
 }
