d19f37a3646019771b84ca46aef4f6e19b5c65f3
liuji <1546243245@qq.com>
2022-11-23 10:31:02 +0800

    modify structure

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"zzzz/myfile"
+)
+
+func main() {
+	fmt.Println("hello world")
+	myfile.FileTest()
+}
--- mytype/type.go
+++ mytype/type.go
@@ -1,4 +1,4 @@
-package main
+package mytype
 
 import "fmt"
 
