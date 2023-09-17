62ff26a3ab807a9e9fea8b2177943c212b4e84d5
liuji <1546243245@qq.com>
2023-09-17 22:27:04 +0800

    time update

--- mytime/mytime.go
+++ mytime/mytime.go
@@ -8,6 +8,7 @@ import (
 
 func TestTime() {
 	t := time.Now()
+	fmt.Println(t.Format("2006-01-02-15-04-05"))
 	u := t.Add(-1 * time.Hour)
 	fmt.Printf("%v\n", t)
 	fmt.Printf("%v\n", u)
@@ -16,6 +17,6 @@ func TestTime() {
 func TestSleepRandomSecond() {
 	rand.Seed(time.Now().UnixNano())
 	start := time.Now()
-	time.Sleep(time.Duration(rand.Intn(100) + 100) * time.Millisecond)
+	time.Sleep(time.Duration(rand.Intn(100)+100) * time.Millisecond)
 	fmt.Printf("Sleep %v millisecond\n", time.Since(start).Milliseconds())
 }
